894ad3599e23963e41ee22a796ab6abbfd7053f6
Fauzan Abrar <fauzanabrar24@gmail.com>
2023-05-26 14:21:56 +0800

    add minify to script

--- README.md
+++ README.md
@@ -5,11 +5,18 @@ requires:
 - node.js
 - npm
 - playwright
+- minify
 
 install package:
 ```
 npm install
 ```
+
+to get minify js file, install:
+```
+npm install -g minify
+```
+
 
 ## How to 
 change url and filename that you want to login in `index.js`:
@@ -24,11 +31,17 @@ npm start
 If you never login before there is a window pop up for login to the website.
 After login auth file will be generated and this is can be used to login with script. 
 then authentication file will be in folder auth and login script will be in folder login.
-copy all of the script in `login/<filename>.js` and paste it to the console (you can try it in incognito browser). 
+copy all of the script in `login/<filename>.js` or `login/<filename>.min.js` and paste it to the console (you can try it in incognito browser). 
 
 Sometimes you need to try other browser.
 
 to try login you need to change url and filename in `/services/tryLogin.js` and run with:
 ```
 npm run try
+```
+
+## Tips
+save the script in bookmark browser with url:
+```
+javascript:<YOUR_SCRIPT_HERE>
 ```
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs        = require('fs');
+const { exec }    = require('child_process');
+
 const produce   = require('./src/services/produceSet');
 const serialize = require('serialize-javascript');
 const getAuth   = require('./src/services/getAuth');
@@ -7,24 +9,26 @@ const url       = '<URL_WITH_HTTP/HTTPS>'
 const filename  = '<FILENAME_WITHOUT_EXTENSION>'
 
 async function runAuto(url, filename) {
-  let authFile  = ''
+  let authFile  = "";
 
   try {
     authFile    = require(`./auth/${filename}.json`);
   } catch {
-    await getAuth(url, filename)
+    await getAuth(url, filename);
     authFile    = require(`./auth/${filename}.json`);
   }
-  
+
   const runFile = produce(authFile);
-  
-  const file    = `const login = ${serialize(runFile)};\n\nlogin.run();`
+
+  const file    = `(function(){const login = ${serialize(runFile)};\n\nlogin.run();})();`;
 
   fs.writeFile(`./login/${filename}.js`, file, (err) => {
     if (err) {
       console.error(err);
       return;
     }
+
+    exec(`minify ./login/${filename}.js > ./login/${filename}.min.js`, (err, stdout, stderr) => { });
 
     console.log(`Running file has been saved to ${filename}.js`);
   });
