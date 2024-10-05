b140a155e6a7ba4e936cb64a40845d87989b3142
Fauzan Abrar <=>
2024-10-06 07:45:16 +0800

    remove redundant code

--- src/services/getAuth.js
+++ src/services/getAuth.js
@@ -1,29 +1,27 @@
-const playwright = require('playwright');
-const fs         = require('fs');
+const playwright = require("playwright");
+const fs = require("fs");
 
 async function getAuth(url, authFilename) {
-  const browser = await playwright.firefox.launch({headless:false});
-  const context = await browser.newContext();
-  const page = await context.newPage();
+	const browser = await playwright.firefox.launch({ headless: false });
+	const context = await browser.newContext();
+	const page = await context.newPage();
 
-  await page.goto(url);
-  
-  await page.waitForTimeout(30000)
-  const storageState = await context.storageState();
-  
-  // Restore the storage state
-  await context.addCookies(storageState.cookies);
-  const myJSON = JSON.stringify(storageState, null, 2);
-  console.log(myJSON);
+	await page.goto(url);
 
-  fs.writeFile(`./auth/${authFilename}.json`, myJSON, (err) => {
-    if (err) {
-      console.error(err, 'yahoo');
-      return;
-    }
-    console.log(`JSON data has been saved to ${authFilename}.json`);
-  });
-  await browser.close();
+	await page.waitForTimeout(70000);
+	const storageState = await context.storageState();
+
+	const myJSON = JSON.stringify(storageState, null, 2);
+	console.log(myJSON);
+
+	fs.writeFile(`./auth/${authFilename}.json`, myJSON, (err) => {
+		if (err) {
+			console.error(err, "yahoo");
+			return;
+		}
+		console.log(`JSON data has been saved to ${authFilename}.json`);
+	});
+	await browser.close();
 }
 
 module.exports = getAuth;
