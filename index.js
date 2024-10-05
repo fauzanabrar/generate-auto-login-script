47f109803d162dd13c760d0bc4c2fb6bf5e19765
Fauzan Abrar <=>
2024-10-05 21:17:23 +0800

    add directory checking

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-const fs        = require('fs');
-const { exec }    = require('child_process');
+const fs = require("fs");
+const { exec } = require("child_process");
 
-const produce   = require('./src/services/produceSet');
-const serialize = require('serialize-javascript');
-const getAuth   = require('./src/services/getAuth');
+const produce = require("./src/services/produceSet");
+const serialize = require("serialize-javascript");
+const getAuth = require("./src/services/getAuth");
 
-const url       = '<URL_WITH_HTTP/HTTPS>'
-const filename  = '<FILENAME_WITHOUT_EXTENSION>'
+const url = "<URL_WITH_HTTP/HTTPS>";
+const filename = "<FILENAME_WITHOUT_EXTENSION>";
 
 async function runAuto(url, filename) {
-  let authFile  = "";
+	// create directory if it doesn't exist
+	if (!fs.existsSync("./auth")) {
+		fs.mkdirSync("./auth");
+	}
 
-  try {
-    authFile    = require(`./auth/${filename}.json`);
-  } catch {
-    await getAuth(url, filename);
-    authFile    = require(`./auth/${filename}.json`);
-  }
+	if (!fs.existsSync("./login")) {
+		fs.mkdirSync("./login");
+	}
 
-  const runFile = produce(authFile);
+	let authFile = "";
 
-  const file    = `(function(){const login = ${serialize(runFile)};\n\nlogin.run();})();`;
+	try {
+		authFile = require(`./auth/${filename}.json`);
+	} catch {
+		await getAuth(url, filename);
+		authFile = require(`./auth/${filename}.json`);
+	}
 
-  fs.writeFile(`./login/${filename}.js`, file, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+	const runFile = produce(authFile);
 
-    exec(`minify ./login/${filename}.js > ./login/${filename}.min.js`, (err, stdout, stderr) => { });
+	const file = `(function(){const login = ${serialize(runFile)};\n\nlogin.run();})();`;
 
-    console.log(`Running file has been saved to ${filename}.js`);
-  });
+	fs.writeFile(`./login/${filename}.js`, file, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+
+		exec(
+			`minify ./login/${filename}.js > ./login/${filename}.min.js`,
+			(err, stdout, stderr) => {},
+		);
+
+		console.log(`Running file has been saved to ${filename}.js`);
+	});
 }
 
-runAuto(url, filename)
+runAuto(url, filename);
